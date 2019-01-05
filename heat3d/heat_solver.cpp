03e2e09b9da7dc9ab2c03a2f27a96ed1f6566bd9
Artsiom Balakir <abalak8@illinois.edu>
2019-01-04 23:00:55 -0600

    init structure, test triangle render

---
+++ heat3d/heat_solver.cpp
@@ -0,0 +1,70 @@
+#include "heat_solver.hpp"
+
+GLuint heat_solver::bind_vertex_location()
+{
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+
+    };
+    
+    GLuint VBO, VAO;
+    
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(glGetAttribLocation(program.get_id(), "aPos"));
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(glGetAttribLocation(program.get_id(), "aColor"));
+
+    program.use_program();
+
+    return VAO;
+}
+
+heat_solver::heat_solver(const shader& program)
+:program(program)
+{
+    auto &parser = heat_parameters::get_instance();
+    // render in domain [-1, 1]^3
+    X = 2 / parser.get_dx();
+    Y = 2 / parser.get_dy();
+    Z = 2 / parser.get_dz();
+
+    posVAO = bind_vertex_location();
+}
+
+void heat_solver::evolve()
+{
+
+}
+
+void heat_solver::update_color_map()
+{
+
+}
+
+void heat_solver::render()
+{
+    glBindVertexArray(posVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glfwSwapBuffers(program.get_window());
+}
+
+double* heat_solver::get_current_state_3channes()
+{
+    return nullptr;
+}
+
+heat_solver::~heat_solver()
+{
+}
---
+++ heat3d/heat_solver.hpp
@@ -0,0 +1,41 @@
+#ifndef HEAT_SOLVER_H
+#define HEAT_SOLVER_H
+
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader.hpp"
+#include "heat_parameters.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+
+class heat_solver
+{
+    size_t X;                // number of cells in Ox
+    size_t Y;                // number of cells in Oy
+    size_t Z;                // number of cells in Oz
+
+    double *state;           // XxYxZ array of current state
+    double *buffer;          // XxYxZ buffer
+    double *heat_map;        // XxyxZx3 array of current state using heat mapping
+
+    const shader &program;   // program to render
+    GLuint posVAO;           // VAO of possitions buffer in glsl
+
+    GLuint bind_vertex_location();
+public:
+    heat_solver(const shader&);
+
+    void evolve();
+    void update_color_map();
+    void render();
+
+    double *get_current_state_3channes();
+
+    ~heat_solver();
+};
+
+
+#endif // HEAT_SOLVER_H
