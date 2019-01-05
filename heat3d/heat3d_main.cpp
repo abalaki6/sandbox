e021c0275d86a7d48a255dbfe0e44a8d1a98d4fc
Artsiom Balakir <abalak8@illinois.edu>
2019-01-04 22:58:07 -0600

    set ups with event loop

--- heat3d/heat3d_main.cpp
+++ heat3d/heat3d_main.cpp
@@ -1,13 +1,81 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include "shader.hpp"
 #include "heat_parameters.hpp"
+#include "heat_solver.hpp"
+#include "camera.hpp"
 
+GLFWwindow* init_gl();
 int main(int argc, char const *argv[]);
+
 
 int main(int argc, char const *argv[])
 {
+    // cli parser
+    heat_parameters::get_instance().parse_cl(argc, argv);
+    // init GLAD and GLFW contenct, set callbacks for free camera
+    GLFWwindow* window = init_gl();
+    camera::use_free_camera(window);
+    // load shaders
+    shader program("shaders/fragment.glsl", "shaders/vertex.glsl");
+    program.set_window(window);
+    // create heat model with provided cli
+    auto model = heat_solver(program);
 
+    glEnable(GL_DEPTH_TEST);  
+    while (!glfwWindowShouldClose(window))
+    {
+        // check for escape event
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, true);
+        }
+        // clean buffers
+        glClearColor(0.f, 0.f, 0.f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // single evolution
+        model.evolve();
+        model.update_color_map();
+        model.render();
+        
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
 
+GLFWwindow* init_gl()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    auto w = heat_parameters::get_instance().get_window_width();
+    auto h = heat_parameters::get_instance().get_window_height();
+    GLFWwindow* window = glfwCreateWindow(w, h, "head3d demo", NULL, NULL);
+    if(window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(-1);
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int w, int h)
+    {
+        glViewport(0, 0, w, h);
+    }
+    );
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        exit(-1);
+    }
+
+    return window;
+}
