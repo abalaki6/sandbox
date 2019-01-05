3056909825945909102fda2fba6638b0a56202e9
Artsiom Balakir <abalak8@illinois.edu>
2019-01-04 20:08:29 -0600

    moved to separate branch (so no Q from RP)

---
+++ heat3d/heat3d_main.cpp
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader.hpp"
+#include "heat_parameters.hpp"
+
+
+int main(int argc, char const *argv[]);
+
+
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
+
---
+++ heat3d/shader.hpp
@@ -0,0 +1,28 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class shader
+{
+    GLuint id;
+    GLuint vertex, fragment; 
+
+    std::string load_file(const std::string file_name);
+    GLuint compile_shader(const char* code, GLenum shader_type);
+    GLuint create_program();
+
+    void check_compilation_errors(GLuint id, bool is_shader);
+public:
+    shader(const std::string fragment_shader_filename, const std::string vertex_shader_filename);
+
+    void use_program();
+};
+
+#endif // SHADER_H
